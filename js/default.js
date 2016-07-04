47778c469ea4f5467b381b9c66059008f14b3c8d
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-07-04 17:20:55 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,13 +6,21 @@ var separate_time = function(time){
   return [sec,min,hours,days];
 }
 
+var update = function(){
 var now = new Date();
 var target = new Date(2016,8,5,0,0,0,0);
 var diff = target.getTime() - now.getTime();
-var counter = separate_time(now);
+var counter = separate_time(diff);
 document.getElementById('countdown').textContent = 
 'リオオリンピックまであと' + 
 counter[3] + '日' +
 counter[2] + '時' + 
 counter[1] + '分' +
 counter[0] + '秒';
+refresh();
+}
+
+var refresh = function(){
+  setTimeout(update,1000);
+}
+update();
