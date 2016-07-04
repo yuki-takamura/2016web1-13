883fa6d3c499dcc35d88964594d8981b5fc62cb1
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-07-04 17:25:44 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,23 +1,23 @@
 var separate_time = function(time){
-  var sec = Math.floor((time / 1000)%60);
-  var min = Math.floor((time / 1000 / 60)%60);
-  var hours = Math.floor((time / 1000 / 60 / 60)%24);
+  var sec = Math.floor((time / 1000) % 60);
+  var min = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
   var days = Math.floor(time / 1000 / 60 / 60 / 24);
   return [sec,min,hours,days];
 }
 
 var update = function(){
-var now = new Date();
-var target = new Date(2016,8,5,0,0,0,0);
-var diff = target.getTime() - now.getTime();
-var counter = separate_time(diff);
-document.getElementById('countdown').textContent = 
-'リオオリンピックまであと' + 
-counter[3] + '日' +
-counter[2] + '時間' + 
-counter[1] + '分' +
-counter[0] + '秒';
-refresh();
+ var now = new Date();
+ var target = new Date(2016,8,5,0,0,0,0);
+ var diff = target.getTime() - now.getTime();
+ var counter = separate_time(diff);
+ document.getElementById('countdown').textContent = 
+ 'リオオリンピックまであと' + 
+ counter[3] + '日' +
+ counter[2] + '時間' + 
+ counter[1] + '分' +
+ counter[0] + '秒';
+ refresh();
 }
 
 var refresh = function(){
