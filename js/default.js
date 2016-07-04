69bcc61efd9d8f5b0101b14b056493aae1beeb4b
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-07-04 18:06:28 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -41,3 +41,46 @@ opt.selected = true;
 document.getElementById('form').select.onchange = function(){
   location.href = document.getElementById('form').select.value;
 }
+
+fuction setCoolie(c_name,value,expiredays){
+  var extime = new Date().getTime();
+  var cltime = new Date(extime + (60*60*24*1000*expiredays));
+  var exdate = cltime.toUTCString();
+  
+  var s = "";
+  s += c_name + "=" + escape(value);
+  s += "; path=" + location.pathname;
+  if(expiredays){
+    s += "; expires = " + exdate+";";
+    }else{
+      s += ";";
+    }
+  
+  document.cookie = s;
+}
+
+fuction getCookie(c_name){
+  var st ="";
+  var ed = "";
+  if(0<document.cookie.length){
+    st = document.cookie.indexOf(c_name + "=");
+    if(st != -1){
+      st = st + c_name.length + 1;
+      ed = document.cookie.indexOf(";",st);
+      if(ed == -1) ed = document.cookie.length;
+      
+      return unescape(document.cookie.substring(st,ed));
+    }
+  }
+  return "";
+}
+
+var last_date = getCookie('lastDate');
+if(last_date){
+  document.getElementById('cookie').textContent = '前回訪れた時間:' + last_date;
+}else{
+  document.getElementById('cookie').textContent = 'はじめまして';
+}
+
+var current_time = new Date();
+setCookie('lastDate',current_time.toString(),7);
