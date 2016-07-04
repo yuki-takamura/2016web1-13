577f719ad02f23cec8118c30248bc35263a608f7
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-07-04 17:37:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -24,3 +24,20 @@ var refresh = function(){
   setTimeout(update,1000);
 }
 update();
+
+function getFileName(){
+  return window.location.href.split('/').pos();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value = "other.html"]');
+}else{
+  opt = document.querySelector('option[value = "index.html"]');
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
+}
